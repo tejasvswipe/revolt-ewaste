6d08926528a472fb52dc7865283b72974aab194f
Tejasv Sahu <tejasvsahu4@gmail.com>
2026-08-22 10:48:11 +0530

    yo

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { trpc } from "@/lib/trpc";
 import { COOKIE_NAME, UNAUTHED_ERR_MSG } from '@shared/const';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";

63ac01bc9897343315d1a0c7f9c1859d5a90ff64
Tejasv Sahu <tejasvsahu4@gmail.com>
2026-08-22 11:28:46 +0530

    do

--- api/trpc/[...path].ts
+++ api/trpc/[...path].ts
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
-  "/api/trpc",
   createExpressMiddleware({
     router: appRouter,
     createContext,
